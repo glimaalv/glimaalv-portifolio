8d9c9bb9389192fcc3c9b76a3bf75c25e4816a16
glimaalv <gusta.pessoal02@gmail.com>
2026-08-31 23:50:44 -0300

    feat: add section about and contact

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,35 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
 
 function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
 
-  if (currentTheme == "light") rootHtml.setAttribute("data-theme", "dark");
-  else rootHtml.setAttribute("data-theme", "light");
+  currentTheme === "dark"
+    ? rootHtml.setAttribute("data-theme", "light")
+    : rootHtml.setAttribute("data-theme", "dark");
 
   toggleTheme.classList.toggle("bi-sunset");
   toggleTheme.classList.toggle("bi-sunrise");
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive
+      ? accordionItem.classList.remove("active")
+      : accordionItem.classList.add("active");
+  });
+});
+
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
